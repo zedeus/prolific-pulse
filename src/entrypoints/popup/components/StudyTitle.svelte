6711638e7233ec7589a754343049b8152f7f33f6
Zed <zedeus@pm.me>
2026-04-19 03:20:42 +0200

    Compose researcher separator via template literal for lint + svelte-check

--- src/entrypoints/popup/components/StudyTitle.svelte
+++ src/entrypoints/popup/components/StudyTitle.svelte
@@ -2,4 +2,4 @@
   let { name, researcherName = '' }: { name: string; researcherName?: string } = $props();
 </script>
 
-{name}{#if researcherName}&#32;<span class="font-normal text-base-content/45">· {researcherName}</span>{/if}
+{name}{#if researcherName}<span class="font-normal text-base-content/45">{` · ${researcherName}`}</span>{/if}
